50a396dd20722010562e1ab7e4930f06abf0ec09
Thuan99 <32856951+Thuan99@users.noreply.github.com>
2017-11-13 19:42:19 +0700

    Add files via upload

---
+++ LV6.2_In ra so nguyen to.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+main()
+{
+	int n,i,j,dem,demlannua=0;
+	printf("nhap n=");
+	scanf("%d",&n);
+	for (i=2;i<n;i++)
+	{
+		
+		dem=0;
+		for (j=1;j<=i;j++)
+		{				
+			if (i%j==0)
+				dem++;			
+		}
+		if (dem==2)
+			demlannua++;					
+	}
+	printf("co %d so nguyen to tu 0 toi %d",demlannua,n);
+}
+
